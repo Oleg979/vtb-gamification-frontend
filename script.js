02c928f9dd36a0849a3cac34999a25fee51f952e
Олег Соловьев <ol.solovev@tinkoff.ru>
2022-10-08 23:56:14 +0400

    test

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ if (progress) {
 }
 
 
-
 function select(e) {
     if (e.target.innerHTML === 'Выбрать') {
         e.target.parentElement.children[2].style.display = 'inline';
         e.target.parentElement.children[3].style.display = 'inline';
-        e.target.innerHTML ='Поблагодарить'
-        e.target.setAttribute('data-bs-dismiss', 'modal')
+        e.target.innerHTML = 'Поблагодарить'
+        e.target.setAttribute('data-bs-dismiss', 'modal');
+    } else if (e.target.innerHTML === 'Поблагодарить') {
+        e.target.setAttribute('data-bs-dismiss', 'modal');
+        sendThanks();
     }
 }
