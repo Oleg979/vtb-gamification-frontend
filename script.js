604c3f18fce69560074c1d84ebb210432f1cd4ee
xKit-22 <78998441+xKit-22@users.noreply.github.com>
2022-10-08 22:24:36 +0400

    init

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ if (progress) {
     progress.style.width = progress.getAttribute("data-complete") + "%";
     progress.style.opacity = 1;
 }
+
+
+
+function select(e) {
+    if (e.target.innerHTML === 'Выбрать') {
+        e.target.parentElement.children[2].style.display = 'inline';
+        e.target.parentElement.children[3].style.display = 'inline';
+        e.target.innerHTML ='Поблагодарить'
+        e.target.setAttribute('data-bs-dismiss', 'modal')
+    }
+}
